06b0d2643cc8b1a4634b05dc97d0c5ba547b1efe
shivapabbu1 <shivaprasad.pabbu@gmail.com>
2024-04-08 16:28:24 +0530

    addedpostmapp

--- demo/src/main/java/com/example/demo/HelloWorldController.java
+++ demo/src/main/java/com/example/demo/HelloWorldController.java
@@ -2,7 +2,11 @@ package com.example.demo;
 
 
 
+import java.util.Map;
+
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,4 +21,8 @@ public class HelloWorldController {
 	        return "hello world"; 
 	    }
 	
+       @PostMapping("create")
+       public String create(@RequestBody Map<String ,String> payload ) {
+    	   return "hello"+ " "+payload.get("name");
+       }
 }
