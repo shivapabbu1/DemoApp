b39a64f0ea7b2665d2004103ea7fd9a84c8077ec
shivapabbu1 <shivaprasad.pabbu@gmail.com>
2024-04-08 11:50:52 +0530

    changed code to restController

--- demo/src/main/java/com/example/demo/HelloWorldController.java
+++ demo/src/main/java/com/example/demo/HelloWorldController.java
@@ -1,19 +1,20 @@
 package com.example.demo;
 
-import org.springframework.stereotype.Controller;
+
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 
 
-@Controller
-@RequestMapping("/demoapp")
+@RestController
+@RequestMapping("/")
 public class HelloWorldController {
 
-       @GetMapping("/helloworld")
+       @GetMapping("helloworld")
        public String getHelloWorld() {
-	        return "helloworld"; 
+	        return "hello world"; 
 	    }
 	
 }
