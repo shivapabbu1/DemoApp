4e1f850ddc95a4c959e9764bfea089d1758c7487
shivapabbu1 <shivaprasad.pabbu@gmail.com>
2024-04-08 11:16:43 +0530

    First commit to DemoApp

---
+++ demo/src/main/java/com/example/demo/HelloWorldController.java
@@ -0,0 +1,19 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+
+@Controller
+@RequestMapping("/demoapp")
+public class HelloWorldController {
+
+       @GetMapping("/helloworld")
+       public String getHelloWorld() {
+	        return "helloworld"; 
+	    }
+	
+}
