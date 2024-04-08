3ac81ae0d147fec50399f2e6f75ed27f3d1f78e8
shivapabbu1 <93253969+shivapabbu1@users.noreply.github.com>
2024-04-08 17:01:39 +0530

    Remote spaces HelloWorldController.java

--- demo/src/main/java/com/example/demo/HelloWorldController.java
+++ demo/src/main/java/com/example/demo/HelloWorldController.java
@@ -1,9 +1,5 @@
 package com.example.demo;
-
-
-
 import java.util.Map;
-
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
