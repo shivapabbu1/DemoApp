61eac745c918209895e1f42237d3036fb6d7c502
shivapabbu1 <shivaprasad.pabbu@gmail.com>
2024-04-09 17:24:01 +0530

    added demo-repo

---
+++ demo-repo/src/main/java/com/demo/base/DemoRepoApplication.java
@@ -0,0 +1,14 @@
+package com.demo.base;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoRepoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoRepoApplication.class, args);
+		System.out.println("hello java ");
+	}
+
+}
